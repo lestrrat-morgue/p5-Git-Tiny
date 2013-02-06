34bed509b8e13e453f31621d100ef4eaa25ca99a
Daisuke Maki <lestrrat+github@gmail.com>
2013-02-06 20:48:24 +0900

    Change how commit objects are parsed

--- lib/Git/Tiny/Object/Commit.pm
+++ lib/Git/Tiny/Object/Commit.pm
@@ -1,10 +1,15 @@
 package Git::Tiny::Object::Commit;
 use strict;
 use base qw(Git::Tiny::Object);
+use Class::Accessor::Lite
+    rw => [ qw(parent tree author committer) ]
+;
 
 sub new {
     my $class = shift;
-    $class->SUPER::new(kind => "commit", @_);
+    my $self = $class->SUPER::new(kind => "commit", @_);
+    $self->parse_content();
+    return $self;
 }
 
 sub parse_content {
@@ -14,25 +19,12 @@ sub parse_content {
     my $index = index($content, "\n");
     while ($index > 0) {
         my ($attr, $value) = split(/\s+/, substr($content, 0, $index, ''), 2);
-        $self->{$attr} = $value;
+        $self->$attr($value);
         substr($content, 0, 1, '');
         $index = index($content, "\n");
     }
-}
-
-BEGIN {
-    foreach my $attr (qw(parent tree author committer)) {
-        eval <<EOM;
-            sub $attr {
-                my (\$self) = \@_;
-                if (! \$self->{$attr}) {
-                    \$self->parse_content();
-                }
-                return \$self->{$attr};
-        }
-EOM
-        die if $@
-    }
+    substr($content, 0, 1, '');
+    $self->content($content);
 }
 
 1;
