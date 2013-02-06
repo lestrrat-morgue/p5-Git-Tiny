e3744eac1ebe1781b1a5fb32a2fcd1881e879099
Daisuke Maki <lestrrat+github@gmail.com>
2013-02-06 17:52:14 +0900

    Implement get_ref, and commit->parent, ->tree, ->author, ->committer

--- lib/Git/Tiny.pm
+++ lib/Git/Tiny.pm
@@ -1,6 +1,8 @@
 package Git::Tiny;
 use strict;
 use Git::Tiny::Pack;
+use Cwd ();
+use File::Find ();
 use File::Spec;
 use Class::Accessor::Lite
     new => 1,
@@ -8,9 +10,58 @@ use Class::Accessor::Lite
 ;
 our $VERSION = '0.01';
 
+sub path_to {
+    my $self = shift;
+    File::Spec->catfile($self->dir, @_);
+}
+
 sub path_to_object {
     my ($self, $sha1) = @_;
-    File::Spec->catfile($self->dir, "objects", substr($sha1, 0, 2), substr($sha1, 2, 38));
+    $self->path_to("objects", substr($sha1, 0, 2), substr($sha1, 2, 38));
+}
+
+sub get_ref_sha1 {
+    my ($self, $name) = @_;
+    my $refs_dir = $self->path_to("refs");
+
+    my $done = 0;
+    my $sha1;
+    File::Find::find({
+        no_chdir => 1,
+        wanted => sub {
+            if ($done) {
+                $File::Find::prune = 1;
+                return;
+            }
+
+            if (!-f $File::Find::name) {
+                return;
+            }
+
+            my $path = File::Spec->abs2rel($File::Find::name, $self->dir);
+            if ($path eq $name) {
+                $sha1 = do {
+                    open my $fh, '<', File::Spec->rel2abs($File::Find::name, $refs_dir) or die;
+                    local $/;
+                    <$fh>
+                };
+                die if $@;
+                $done = 1;
+                return;
+            }
+        }
+    }, $refs_dir);
+
+    return $sha1 || ();
+}
+
+sub get_ref {
+    my ($self, $name) = @_;
+    my $sha1 = $self->get_ref_sha1($name);
+    if (! $sha1) {
+        Carp::confess("Could not find ref $name");
+    }
+    return $self->get_object( $sha1 );
 }
 
 sub get_object {
@@ -36,5 +87,6 @@ Git::Tiny - Minimalistic Pure-Perl Git Implementation
     my $git = Git::Tiny->new(dir => "/path/to/git");
 
     $git->get_object($sha1);
+    $git->get_ref("refs/heads/master");
 
 =cut
--- lib/Git/Tiny/Object/Commit.pm
+++ lib/Git/Tiny/Object/Commit.pm
@@ -7,4 +7,32 @@ sub new {
     $class->SUPER::new(kind => "commit", @_);
 }
 
+sub parse_content {
+    my $self = shift;
+    my $content = $self->content;
+
+    my $index = index($content, "\n");
+    while ($index > 0) {
+        my ($attr, $value) = split(/\s+/, substr($content, 0, $index, ''), 2);
+        $self->{$attr} = $value;
+        substr($content, 0, 1, '');
+        $index = index($content, "\n");
+    }
+}
+
+BEGIN {
+    foreach my $attr (qw(parent tree author committer)) {
+        eval <<EOM;
+            sub $attr {
+                my (\$self) = \@_;
+                if (! \$self->{$attr}) {
+                    \$self->parse_content();
+                }
+                return \$self->{$attr};
+        }
+EOM
+        die if $@
+    }
+}
+
 1;
